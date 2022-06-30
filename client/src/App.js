7ca4a5bd8c6ee0f1b6efd13ec6ed3517b27505e4
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-06-30 00:39:35 -0500

    forgot to add all for previous commits but the changes made were adding apollo client to App.js. Schema folder was created as well as typedefs and resolvers. most of the server is built.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// HTTP Link to local server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+//Authlink for verification 
+const authlink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Instance of ApolloClient
+const client = new ApolloClient({
+  link: authlink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}> 
     <Router>
       <>
         <Navbar />
@@ -16,6 +41,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
