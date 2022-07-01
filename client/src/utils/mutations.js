427445a8cc18b857d41342150a21e8961389670a
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-06-30 21:29:48 -0500

    fixed syntax error for mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ mutation loginx($email: String!, $password: String!) {
 `;
 
 export const ADD_USER = gql`
-mutation addUser($username: String!, email: String!, password: String!) {
+mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
         token
         user{
